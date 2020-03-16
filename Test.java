dd3a91b7ecc97c92e6b332cc376c572b3be4dfad
Gix <itsgix01@gmail.com>
2020-03-16 19:36:14 -0400

    asked a very important question

--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@
 */
 
 public class Test{
+  // Why do they call it oven when you of in the cold food of out hot eat the food
   public void main(String[] args){
     System.out.println("Hello World!");
   }
